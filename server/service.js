8103f4631df8a3925a1bcbf84eba00266c01467c
dilanSuraj <it17121002@my.sliit.lk>
2020-10-15 03:21:38 +0530

    Unregister Services

--- server/service.js
+++ server/service.js
@@ -27,7 +27,16 @@ module.exports = (config) => {
     return res.json({ result: serviceKey });
   });
 
-  service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => next('Not implemented'));
+  service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res) => {
+    const { servicename, serviceversion, serviceport } = req.params;
+    // Check for ipv6 and changed the service ip format
+    const serviceip = req.connection.remoteAddress.includes('::') ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+    const serviceKey = serviceRegistry
+      .unRegister(servicename, serviceversion, serviceip, serviceport);
+
+    return res.json({ result: serviceKey });
+  });
 
   service.get('/find/:servicename/:serviceversion', (req, res, next) => next('Not implemented'));
 
